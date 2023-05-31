7e6ea9ea03fd6991726a64819a652dcf43a9d5fe
softwere123 <84305921+softwere123@users.noreply.github.com>
2023-05-31 13:10:56 +0900

    ui selecte window
    ui 선택창만듬

--- xr project elmental_1/Assets/Settherobottype.cs
+++ xr project elmental_1/Assets/Settherobottype.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class Settherobottype : RobotController10
+
+public class Settherobottype : MonoBehaviour 
 {
-    public ActionBasedSnapTurnProvider snapturn;
-    public ActionBasedContinuousTurnProvider
+    //public ActionBasedSnapTurnProvider snapturn;
+    //public ActionBasedContinuousTurnProvider continiousTurn;
     
+    public RobotController10 fire;
+    public Earth earth;
+
+    // 턴 방식 바꾸는법
     public void SetTypeFromIndex(int index)
     {
+        if(index == 0)
+        {
+            fire.enabled = false;
+            earth.enabled = true;
+            
+        }
+        else if(index == 1)
+        {
+           
+           fire.enabled = true;
+            earth.enabled = false;
+           
+        }
 
     }
 
